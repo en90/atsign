1a8fb94e215f24cfe3285587d2385b2a61d36ab9
Martin Stensgård <mastensg@ping.uio.no>
2013-08-14 09:11:57 +0200

    keep dictionaries to speed up searches
    Signed-off-by: Martin Stensgård <mastensg@ping.uio.no>

--- atsign.sh
+++ atsign.sh
@@ -1,55 +1,131 @@
 #!/bin/bash
- 
-function atsign()
+
+ATSIGN_CACHE="$HOME/.cache/atsign"
+
+atsign_index()
 {
-  if [ $? = 127 ]
-  then
-    local PROGRAM=$(history 2 | head -n 1 | tr -s \  \ | cut -f3 -d\  | tr -d '[:space:]')
-    local PATTERN="/s?bin/$PROGRAM\$"
-    local SHORT_PATTERN="$PROGRAM"
-  elif [ $1 != "" ]
-  then
+    awk '
+    {
+        split($1, packages, ":")
+        package = packages[1]
+
+        for (i = 2; i <= NF; ++i)
+        {
+            last = split($i, path, "/")
+            file = path[last]
+
+            if (index(dict[file], package))
+                continue
+
+            dict[file] = (dict[file] " " package)
+        }
+    }
+
+    END {
+        for (x in dict)
+            print x ":" dict[x]
+    }
+    ' | LC_COLLATE=C sort
+}
+
+atsign_cache()
+{
+    echo "Indexing $1 files..." 1>&2
+
+    apt-file --regexp search "$2" | atsign_index > "$ATSIGN_CACHE/$1"
+}
+
+atsign_update()
+{
+    mkdir -p "$ATSIGN_CACHE"
+
+    apt-file update 1>&2
+
+    atsign_cache all '.'
+    atsign_cache bin '(^|.*/)(s?bin|games|lib)/[^/\s]+$'
+    atsign_cache lib '/lib[^/\s]+\.(a|so)[^/\s]*$'
+}
+
+atsign_search()
+{
+    local DICT="$ATSIGN_CACHE/$1"
+
+    test -d "$ATSIGN_CACHE" || atsign_update
+
     case "$1" in
-      -l*)
-        local FILE="lib$(echo "$1" | sed 's/^-l//').(so|a)"
-        ;;
-      *)
-        local FILE="$1"
+        all)
+            local PATTERN=".*$2.*"
+            ;;
+        bin)
+            local PATTERN="$2"
+            ;;
+        lib)
+            local PATTERN="$2\.(a|so)"
+            ;;
+        *)
+            echo "what?" 1>&2
+            return 1
     esac
-    local PATTERN="/$FILE\$"
-    local SHORT_PATTERN="$FILE"
-  fi
- 
-  local BUFFER=$(mktemp)
- 
-  (
-    apt-file -x search "$PATTERN" > "$BUFFER"
- 
-    local COUNT=$(wc -l "$BUFFER" | cut -f1 -d\ )
- 
+
+    look -b "$2" "$DICT" | grep -P "^$PATTERN: " | atsign_index | column -t
+}
+
+atsign()
+{
+    if [ $? = 127 ]
+    then
+        local DICT=bin
+        local FILE=$(history 2 | head -n 1 | tr -s \  \ | cut -f3 -d\  | tr -d '[:space:]')
+    elif [ x"" != x"$1" ]
+    then
+        case "$1" in
+            -u)
+                atsign_update
+                return
+                ;;
+            -l*)
+                local DICT=lib
+                local FILE="lib$(echo "$1" | sed 's/^-l//')"
+                ;;
+            *)
+                local DICT=all
+                local FILE="$1"
+        esac
+    else
+        echo no 1>&2
+        return 1
+    fi
+
+    local BUFFER=$(mktemp)
+
+    (
+    atsign_search "$DICT" "$FILE" > "$BUFFER" || return 1
+
+    local COUNT=$(wc -l "$BUFFER" | cut -d' ' -f1)
+
     case $COUNT in
-      0)
-        echo "$SHORT_PATTERN: no match" 1>&2
-        exit 1
-        ;;
-      1)
-        cat "$BUFFER"
-        local CHOICE=1
-        ;;
-      *)
-        nl "$BUFFER"
-        echo -n "> "
-        read CHOICE || exit 1
-        expr \( "$CHOICE" '>' 0 \) \& \( "$CHOICE" '<=' $COUNT \) >/dev/null || exit 1
-        ;;
+        0)
+            echo "$FILE: no match" 1>&2
+            exit 1
+            ;;
+        1)
+            cat "$BUFFER"
+            local CHOICE=1
+            ;;
+        *)
+            nl "$BUFFER"
+            echo -n "> "
+            local CHOICE=$(head -1)
+            expr \( "$CHOICE" '>' 0 \) \& \( "$CHOICE" '<=' $COUNT \) >/dev/null || exit 1
+            ;;
     esac
- 
+
     local PACKAGE=$(sed "$CHOICE!D" "$BUFFER" | cut -d: -f1)
- 
-    /usr/bin/sudo /usr/bin/aptitude -P install "$PACKAGE"
-  )
- 
-  rm -f "$BUFFER"
+
+    /usr/bin/sudo /usr/bin/aptitude --prompt install "$PACKAGE"
+    )
+
+    rm -f "$BUFFER"
 }
 
 alias @=atsign
